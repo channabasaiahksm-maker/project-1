44db22f567ea2fc9fca2e5900d121c3188cbd5cc
channabasaiahksm-maker <channabasaiahksm@gmail.com>
2026-09-09 13:10:34 +0530

    creating new project

---
+++ src/main/java/com/project_1/devops/Terraform.java
@@ -0,0 +1,4 @@
+package com.project_1.devops;
+
+public class Terraform {
+}
---
+++ src/test/java/com/project_1/devops/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_1.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
